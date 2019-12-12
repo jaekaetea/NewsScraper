5c95ab58dc191aa0d9cc824e2372f97ee253d41b
JoJo <jae.kae.tea@gmail.com>
2019-12-11 21:18:08 -0600

    Changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(logger("dev"));
